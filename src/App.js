8303b7ca3eaec1fa2c8f59b3fb8d3d611dab0638
lucasxz <zhou1008@purdue.edu>
2019-11-12 00:06:44 +0000

    router added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div class="container">
-
-
-
-
+    <div>
+      <Navbar/>
+      <switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/profile" component={Profile} />
+      </switch>
     </div>
   )
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Link } from 'react-router-dom';
+
+class Navbar extends React.Component {
+    render() {
+        return (
+            <nav class="navbar is-transparent is-dark container" role="navigation" aria-label="main navigation" style={{backgroundColor: null, backgroundImage: null}}>
+                <div class="navbar-brand">
+                    <a class="navbar-item" href="">
+                        devCast
+      </a>
+
+                    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                        <span aria-hidden="true"></span>;
+        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </a>
+                </div>
+
+                <div id="navbarBasicExample" class="navbar-menu">
+                    <div class="navbar-start">
+
+                    </div>
+                    <div class="navbar-end">
+                        <div class="navbar-item">
+                            <Link to="/profile">profile</Link>
+                        </div>
+                        <div class="navbar-item">
+                            <Link to="/">home</Link>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        );
+    };
+}
+
+export default Navbar;
